e3ab158514cfea19530030267327c485424590da
Hellen <99354447+HellenTs@users.noreply.github.com>
2022-04-29 01:31:07 +0300

    Asdf

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef BIT_MANIPULATION_H_
+#define BIT_MANIPULATION_H_
 
-int _putchar(char c);
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +13,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif
+#endif /* #ifndef BIT_MANIPULATION_H_ */
